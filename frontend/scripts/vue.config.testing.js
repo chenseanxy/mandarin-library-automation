b8c65d0adf0dc93ccbcbed690ae466e68ac3c3ed
Xinyang Chen <chenxy1999728@126.com>
2020-02-23 04:13:52 +0800

    Fixed "Host Header" error by adding allowedhosts

--- frontend/scripts/vue.config.testing.js
+++ frontend/scripts/vue.config.testing.js
@@ -1,5 +1,7 @@
 module.exports = {
     devServer: {
-        proxy: 'http://api:5000'
-      }
-  }
+        proxy: 'http://api:5000',
+        port: '8080',
+        allowedHosts: ['testrunner']
+    }
+}
