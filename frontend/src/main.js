1986b561322a4a0ede349892756b4df29b3c71ec
Jinwboo <1248144457@qq.com>
2020-04-23 11:00:35 +0800

    noww

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,3 @@
-/*
-import Vue from 'vue'
-import VueBarcode from 'vue-barcode';
-import App from './App.vue'
-import router from './router/index.js'
-import './plugins/element.js'
-import './assets/theme/index.css'
-import './assets/css/global.css'
-Vue.config.productionTip = false
-Vue.component('barcode', VueBarcode);
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
-*/
 import Vue from 'vue'
 import VueBarcode from 'vue-barcode';
 import App from './App.vue'
