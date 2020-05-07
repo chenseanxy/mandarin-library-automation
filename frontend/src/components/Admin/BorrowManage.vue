53f4aec661d420548480e85f212962d86f28334c
samulenzz <samule_online@126.com>
2020-05-07 23:07:31 +0800

    设置了修改罚金，归还日期，预约金的界面。但是没有写按钮的方法

--- frontend/src/components/Admin/BorrowManage.vue
+++ frontend/src/components/Admin/BorrowManage.vue
@@ -1,8 +1,66 @@
 <template>
   <div class="outer">
-    <div class="block">
-      
+    <!--导航栏-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>Admin Homepage</el-breadcrumb-item>
+      <el-breadcrumb-item>Borrow&Return management</el-breadcrumb-item>
+      <!-- <el-breadcrumb-item>Registered accounts</el-breadcrumb-item> -->
+    </el-breadcrumb>
 
+    <div class="block">
+      <!--用来设置悬浮黑色框-->
+      <div class="info_box">
+        <!--标题-->
+        <h2>Manage</h2>
+        <!--表单区域-->
+
+        <el-row :gutter="5">
+          <el-col :span="12">
+            <div>
+              <el-tag type="success">Fine</el-tag>
+            </div>
+          </el-col>
+          <el-col :span="12">
+            <p class="count-fine">$ {{fineValue}} /day</p>
+          </el-col>
+        </el-row>
+
+        <el-row :gutter="5">
+          <el-col :span="12">
+            <div>
+              <el-tag type="success">Period</el-tag>
+            </div>
+          </el-col>
+          <el-col :span="12">
+            <p class="count-returnPeriod">{{returnPeriod}} days</p>
+          </el-col>
+        </el-row>
+
+        <el-row :gutter="5">
+          <el-col :span="12">
+            <div>
+              <el-tag type="success">Depoist</el-tag>
+            </div>
+          </el-col>
+          <el-col :span="12">
+            <p class="count-depoist">$ {{depoist}}</p>
+          </el-col>
+        </el-row>
+
+        <el-row :gutter="3" class="btns">
+          <!-- <el-col :span="8">
+            <div class="grid-content bg-purple-dark"></div>
+          </el-col> -->
+          <el-col :span="6">
+            <el-button type="primary" icon="el-icon-edit" size="big" @click="edit">Edit </el-button>
+          </el-col>
+          <el-col :span="10">
+            <el-button type="danger" icon="el-icon-close" size="big" @click="cancel">Cancel</el-button>
+          </el-col>
+        </el-row>
+
+
+      </div>
     </div>
   </div>
 </template>
@@ -11,8 +69,10 @@
 export default {
   data() {
     return {
-      fit: "cover",
-      src: require("../../assets/susan.jpg")
+      //罚金，归还期限，保证金
+      fineValue: 1,
+      returnPeriod: 30,
+      depoist: 300
     };
   }
 };
@@ -23,9 +83,17 @@ export default {
   height: 100%;
   width: 100%;
 }
+
+//面包屑
+.el-breadcrumb {
+  /*面包屑下拉间隙*/
+  margin-bottom: 15px;
+}
+
+//带图片区域
 .block {
   background-color: #2b4b6b;
-  background: url("../../assets/sylvia.jpg") no-repeat center center fixed;
+  background: url("../../assets/susan.jpg") no-repeat center center fixed;
   /* background-size: 100% 100%; */
   background-size: cover;
   /* background-repeat: no-repeat; */
@@ -36,5 +104,113 @@ export default {
   -o-background-size: cover;
 }
 
+//黑盒子
+.info_box {
+  /*让盒子进行位移*/
+  // position: absolute;
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 
+  height: 500px;
+  width: 400px;
+  padding: 40px;
+  background: rgba(0, 0, 0, 0.8);
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+}
+
+.info_box h2 {
+  //这个margin再说吧
+  margin: 0 0 30px;
+  padding: 0;
+  text-align: center;
+  color: #fff;
+}
+
+.el-tag {
+  height: 40px;
+  width: 100px;
+  font-size: larger;
+  font-weight: bold;
+  // text-align: center;
+  // align-content: center;
+  // align-items: center;
+  margin-bottom: 15px;
+  margin-top: 12px;
+}
+//输入文本框的样式，需要修改
+.login_form {
+  position: absolute;
+  //position: relative;
+  // bottom: 60px;
+  // width: 100%;
+  // padding: 0 20px;
+  // box-sizing: border-box;
+}
+//按钮的样式
+.btns {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 40px;
+  // position: relative;
+  // bottom: 0;
+}
+
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+.el-col {
+  border-radius: 4px;
+}
+
+.count-fine {
+  font-size: xx-large;
+  font-weight: bolder;
+  color: #409eff;
+  text-align: center;
+  margin-bottom: 15px;
+  margin-top: 12px;
+}
+
+.count-depoist {
+  font-size: xx-large;
+  font-weight: bolder;
+  color: #d9e626;
+  text-align: center;
+  margin-bottom: 15px;
+  margin-top: 12px;
+}
+
+.count-returnPeriod {
+  font-size: xx-large;
+  font-weight: bolder;
+  color: #ff5d40;
+  text-align: center;
+  margin-bottom: 15px;
+  margin-top: 12px;
+}
+
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
 </style>
