a65fb1bf07ca5c4c8bdd4b00f4cfdc46927925d9
leslieranaa <2524110204@qq.com>
2020-06-11 07:55:29 +0800

    信息修改样式

--- frontend/src/components/Reader/ModifyPersonalInformation.vue
+++ frontend/src/components/Reader/ModifyPersonalInformation.vue
@@ -1,38 +1,29 @@
 <template>
   <div>
-    <el-container>
-    <el-main>
+    <div class="change_box">
+      <el-form ref="form" :model="form" label-width="80px" class="hhform">
+        <el-form-item label="username">
+          <el-input v-model="form.username" :disabled="form.kw0"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click=" form.kw0 = !form.kw0">修改</el-button>
+        </el-form-item>
 
+        <el-form-item label="email">
+          <el-input v-model="form.email" :disabled="form.kw1"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click=" form.kw1 = !form.kw1">修改</el-button>
+        </el-form-item>
 
-<el-form ref="form" :model="form" label-width="80px" class="hhform">
-  <el-form-item label="username" >
-    <el-input v-model="form.username" :disabled="form.kw0"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click=" form.kw0 = !form.kw0">修改</el-button>
-  </el-form-item>
-
-  <el-form-item label="email">
-    <el-input v-model="form.email" :disabled="form.kw1"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click=" form.kw1 = !form.kw1">修改</el-button>
-  </el-form-item>
-
-    <el-form-item label="position">
-    <el-input v-model="form.position" :disabled="form.kw2"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click=" form.kw2 = !form.kw2">修改</el-button>
-  </el-form-item>
-
-
-
-
-</el-form>
-
-    </el-main>
-    </el-container>
+        <el-form-item label="position">
+          <el-input v-model="form.position" :disabled="form.kw2"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click=" form.kw2 = !form.kw2">修改</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -40,7 +31,7 @@
 export default {
   data() {
     return {
-      form:{
+      form: {
         username: "user1",
         email: "123456@126.com",
         position: "teacher",
@@ -51,16 +42,27 @@ export default {
     };
   },
 
-  methods: {
-
-  }
+  methods: {}
 };
 </script>
 
 <style scoped>
 .hhform {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 35px;
+  box-sizing: border-box;
   width: 300px;
 }
-
-
+.change_box {
+  width: 450px;
+  height: 400px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  position: absolute;
+  left: 55%;
+  top: 55%;
+  transform: translate(-50%, -50%);
+}
 </style>
